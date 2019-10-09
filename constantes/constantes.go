f320fa01132cf84812e1c61544554dbe6f1a857f
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-10-09 16:24:52 +0200

    change in message class

--- constantes/constantes.go
+++ constantes/constantes.go
@@ -1,9 +1,11 @@
 package constantes
 
+import "time"
+
 const MulticastAddr = "224.0.0.1:6666"
 const ListeningPort = ":6667"
 
-const AttenteK int = 3
+const AttenteK time.Duration = 3
 
 const SYNC uint8 = 0
 const FOLLOW_UP uint8 = 1
