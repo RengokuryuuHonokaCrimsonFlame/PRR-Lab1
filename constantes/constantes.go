9240af80b5bd173dfbee9901544a46f348d67c83
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-10-10 08:03:49 +0200

    Made changes on constantes class

--- constantes/constantes.go
+++ constantes/constantes.go
@@ -6,8 +6,8 @@ const MulticastAddr = "224.0.0.1:6666"
 const ListeningPort = ":6667"
 
 const AttenteK time.Duration = 3
-const Min uint8 = 4
-const Max uint8 = 60
+const Min int8 = 4
+const Max int8 = 60
 
 const SYNC uint8 = 0
 const FOLLOW_UP uint8 = 1
